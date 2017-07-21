c39df9e468c047409456fd6ee8c57137ed5bafeb
lspgithub <3097881265@qq.com>
2017-07-21 12:12:45 +0800

    修改主页bug

--- yjf_hou/frontend/controllers/CommonController.php
+++ yjf_hou/frontend/controllers/CommonController.php
@@ -21,9 +21,25 @@ class CommonController extends Controller
     public function init()
     {
         parent::init();
-
         if(empty(Yii::$app->session['username'])){
             echo "<script>alert('请先登录');location.href='?r=login/login';</script>";
         }
+        $this->rbac();
+
+    }
+    //权限控制
+    public function rbac(){
+        $controller=$this->id;
+        $action=$this->action;
+        if(!empay()){}
+        echo $controller."和".$action;die;
+        $db=Yii::$app->db;
+        $username=Yii::$app->session['username'];
+        $sql="select u_id from fang_user where username='{$username}'";
+        $id=$db->createCommand($sql)->queryOne();
+        var_dump($id);die;
+        $charaSql="select c_id from fang_userchara where u_id={$id}";
+
+
     }
 }
