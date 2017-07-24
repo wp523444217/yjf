ef6c9ae04e2907f6baeafc5339273801a4d0ce23
new yjg <1953312280@qq.com>
2017-07-24 13:39:11 +0800

    前台首页数据展示

--- yjf_qian/app/Http/Controllers/Home/IndexController.php
+++ yjf_qian/app/Http/Controllers/Home/IndexController.php
@@ -11,6 +11,9 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return view('home/index/index');
+    	$user= DB::table('fang_zaixian')->limit(4)->get(); //网游竞技
+    	$info =  DB::table('fang_zhunze')->get();
+    	// print_r($info);die;
+        return view('home/index/index')->with(['user'=>$user,'info'=>$info]);
     }
 }
